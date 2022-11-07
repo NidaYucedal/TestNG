b682fa658520c980e5771ff99be49e285705bb61
Nida Yucedal <yucedalnida@gmail.com>
2022-11-07 13:15:28 +0300

    new

--- src/test/java/tests/ListenersTest01.java
+++ src/test/java/tests/ListenersTest01.java
@@ -15,7 +15,7 @@ public class ListenersTest01 {
     @Test
     public void test2() {
         System.out.println("Test Case 2 - FAILED");
-        Assert.assertTrue(false);//f
+        Assert.assertTrue(false);//fa
     }
 
     @Test
